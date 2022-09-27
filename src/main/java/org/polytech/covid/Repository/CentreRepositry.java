f59a7c2f29bf754127de91514672d806c602ebe3
EmyLIEUTAUD <emylieutaud@gmail.com>
2022-09-27 15:39:45 +0200

    Mise à jour des classes Centre et CentreDAO après merge de la branche Yunqiao

--- src/main/java/org/polytech/covid/Repository/CentreRepositry.java
+++ src/main/java/org/polytech/covid/Repository/CentreRepositry.java
@@ -2,10 +2,13 @@ package org.polytech.covid.Repository;
 
 import org.polytech.covid.Entity.Centre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CentreRepositry extends JpaRepository<Centre,Integer> {
+@Repository
+public interface CentreRepositry extends JpaRepository<Centre, Integer> {
+
     List<Centre> findByVilleId(Integer villeId);
 
 }
