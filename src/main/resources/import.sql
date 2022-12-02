5f014373368bb39cf5a58ccae1df039798ccdf29
EmyLIEUTAUD <emylieutaud@gmail.com>
2022-12-02 17:02:09 +0100

    import.sql commenté

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
+/*
+
 -- Ajout des centres dans la bdd --
 COPY Centre(gid,nom,arrete_pref_numero,xy_precis,id_adr,num_adresse,adresse,cp,com_insee,comnom,lat_coor1,long_coor1,structure_siren,structure_type,structure_rais,structure_num,structure_voie,structure_cp,structure_insee,structure_com,_userid_creation,_userid_modification,_edit_datemaj,lieu_accessibilite,horaires_lundi,horaires_mardi,horaires_mercredi,horaires_jeudi,horaires_vendredi,horaires_samedi,horaires_dimanche,rdv,date_fermeture,date_ouverture,rdv_site_web,rdv_tel,rdv_tel2,rdv_modalites,rdv_consultation_prevaccination,centre_svi_repondeur,centre_fermeture,reserve_professionels_sante,centre_type) FROM 'D:\Polytech\5A\Fullstack\Projet\covid-api\src\main\resources\centres-vaccination.csv' DELIMITER ';' CSV HEADER;
 
@@ -24,3 +26,5 @@ INSERT INTO Medecin (identifiant, gid) VALUES (4, 188);
 -- Ajout d'un personne public dans la bdd --
 INSERT INTO Personne (nom, prenom, mail) VALUES ('public1', 'public1', 'public1@gmail.com');
 INSERT INTO Public (identifiant, dose) VALUES (5, 2)
+
+*/
